151a4a573e62d6939b2187d3ac666fc0d4c69297
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-04-07 11:01:25 -0300

    Calculo da media

---
+++ backend/src/app/services/statistics.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import statistics as st
+
+
+def mean(data):
+    """
+
+
+    """
+    data_df = pd.DataFrame(data)
+    if data_df.shape[1] > 0:
+        avg = data.mean()
+    else:
+        raise ValueError(f"Empty data received. data should have shape (1,1) or higher and it has {data_df.shape}")
+    return avg
+
+
+
