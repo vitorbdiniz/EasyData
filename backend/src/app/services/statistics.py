6593e44ebbfbc1673c6c1ac4ec0ff72c289abc21
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-04-07 11:02:17 -0300

    Calculo da mediana

--- backend/src/app/services/statistics.py
+++ backend/src/app/services/statistics.py
@@ -16,4 +16,17 @@ def mean(data):
     return avg
 
 
+def median(data):
+    """
+
+
+    """
+    data_df = pd.DataFrame(data)
+    if data_df.shape[1] > 0:
+        avg = data.median()
+    else:
+        raise ValueError(f"Empty data received. data should have shape (1,1) or higher and it has {data_df.shape}")
+    return avg
+
+
 
