5715906a5447247d546b9b8ebe22777bcf18e68e
Adriano <adriano.araujo@ccc.ufcg.edu.br>
2021-04-23 11:04:37 -0300

    Extract users app

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
-from apps.core.views import Register, Authenticate
 
 urlpatterns = [
     # auth
-    path('users/register', Register.as_view(), name='register'),
-    path('users/authenticate', Authenticate.as_view(), name='authenticate'),
 ]
--- easydata/urls.py
+++ easydata/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.core.urls')),
+    path('api/v1/users/', include('apps.users.urls')),
 ]
