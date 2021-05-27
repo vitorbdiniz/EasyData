fe99e330d26b98d7ec9cff048e14f68ad1deb675
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-05-27 14:48:53 -0300

    Acrescenta tratamento de erro

--- apps/core/services/data_conversion.py
+++ apps/core/services/data_conversion.py
@@ -14,7 +14,20 @@ def json_to_df(data):
 
 
 def csv_to_df(data):
-    return pd.read_csv(data)
+
+    try:
+        result = pd.read_csv(data)
+    except:
+        result = ''' Certifique-se de que o arquivo enviado está de acordo com os seguintes requisitos:
+        1. O arquivo enviado deve ser do tipo ".csv" e suas colunas devem ser separadas por vírgulas.
+        2. Todas as colunas do arquivo devem começar na linha um.
+        3. A primeira linha do arquivo deve conter os nomes das colunas presentes.
+        4. Todas as colunas, com excessão do nome, devem ser numéricas, garantindo que as estatísticas sejam calculadas corretamente.
+        5. Não devem existir na planilha dados incoerentes e que não façam parte do conjunto de dados a ser analisado.
+        '''
+        
+
+    return result
 
 
 def df_to_json(data):
