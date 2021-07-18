2657943e5669b968e24e5f56d7ad505e19bdbf72
Adriano <adriano.araujo@ccc.ufcg.edu.br>
2021-07-18 15:41:47 -0300

    Adiciona mensagem de erro esqueci a senha

--- apps/users/views.py
+++ apps/users/views.py
@@ -61,7 +61,7 @@ def forgot(request):
                 message = "Clique aqui para resetar sua senha: %s" % uri
                 _send_mail_plain_text(message, email)
             except:
-                messages.error(request, 'Esse email não está cadastrado em nossa base. :(')
+                messages.error(request, 'Esse email não está cadastrado em nossa base.')
                 return redirect('forgot_password')
             messages.success(request, 'Email enviado com sucesso para ' + email)
             return redirect('login')
